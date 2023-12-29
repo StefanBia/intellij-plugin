39ccbb0e41464eb599c8e2cecb6d8d78a89254fc
Stefan Bia <stefanbia0108@gmail.com>
2023-12-29 10:38:57 +0200

    Replace Message Box to JFrame

--- src/main/kotlin/com/example/demo/HelloWorldAction.kt
+++ src/main/kotlin/com/example/demo/HelloWorldAction.kt
@@ -2,10 +2,46 @@ package com.example.demo
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.ui.Messages
+import java.awt.*
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+
 
 class HelloWorldAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        Messages.showInfoMessage("Hello world!", "Info")
+        val frame = JFrame("Hello World Frame")
+        val label = JLabel("Hello World!")
+        val closeButton = JButton("OK")
+
+        frame.layout = BorderLayout()
+
+        val labelPanel = JPanel(GridBagLayout())
+        val labelConstraints = GridBagConstraints()
+        labelPanel.add(label, labelConstraints)
+
+        val buttonPanel = JPanel(FlowLayout(FlowLayout.CENTER))
+        buttonPanel.add(closeButton)
+
+        frame.add(labelPanel, BorderLayout.CENTER)
+        frame.add(buttonPanel, BorderLayout.SOUTH)
+
+
+        // Set background color of the frame
+        frame.contentPane.background = Color.decode("#DDC7A0")
+
+        frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+        frame.setSize(300, 200)
+        frame.setLocationRelativeTo(null) // Open in the middle of the screen
+        frame.isVisible = true
+
+        closeButton.addActionListener { frame.dispose() }
+    }
+
+    internal class JButtonPanel : JPanel() {
+        init {
+            layout = FlowLayout(FlowLayout.CENTER)
+        }
     }
 }
